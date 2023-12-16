2b24440d45dc4a1b77d9d0ffd61fff4ee7b4ea4b
John Schock <joschock@microsoft.com>
2023-12-15 23:23:05 -0800

    fun with rayon and crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use std::collections::BTreeSet;
 use std::fs::read_to_string;
+use std::sync::Mutex;
+use std::thread::sleep;
+use std::time::Duration;
+use rayon::prelude::*;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+use crossterm::ExecutableCommand;
+use crossterm::cursor;
+use crossterm::style;
+use crossterm::style::Stylize;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 enum Direction {
     Up,
     Down,
@@ -54,41 +64,65 @@ fn next_direction(current_tile: char, entered_from: Direction) -> Vec<Direction>
     }
 }
 
-fn follow_beam(mirror_map: &Vec<Vec<char>>, light_map: &mut Vec<Vec<Vec<Direction>>>, x: usize, y: usize, entered_from: Direction) {
-    for direction in next_direction(mirror_map[y][x], entered_from) {
-        if !light_map[y][x].contains(&direction) {
-            light_map[y][x].push(direction.clone());
+fn follow_beam(mirror_map: &Vec<Vec<char>>, light_map: &Mutex<Vec<Vec<BTreeSet<Direction>>>>, x: usize, y: usize, entered_from: Direction, print: bool) {
+    if print {
+        let _guard = light_map.lock().unwrap();
+        let mut stdout = std::io::stdout();
+        let _ = stdout.execute(cursor::MoveTo(x as u16,y as u16));
+        let _ = stdout.execute(style::PrintStyledContent(" ".with(style::Color::White).on(style::Color::Red)));
+    }
+    if print {
+        sleep(Duration::from_micros(1));
+    }
+    let next_directions = next_direction(mirror_map[y][x], entered_from);
+    next_directions.par_iter().for_each(|direction|{
+        let contains = {
+            let guard = light_map.lock().unwrap();
+            guard[y][x].contains(&direction)
+        };
+        if !contains {
+            {
+                light_map.lock().unwrap()[y][x].insert(direction.clone());
+            }
             match direction {
                 Direction::Up => {
                     if y == 0 {
-                        continue;
+                        return;
                     }
-                    follow_beam(mirror_map, light_map, x, y-1, Direction::Down);
+                    stacker::maybe_grow(0x8000, 0x100000, ||{
+                        follow_beam(mirror_map, light_map, x, y-1, Direction::Down, print);
+                    });
                 },
                 Direction::Down => {
                     if y == mirror_map.len() - 1 {
-                        continue;
+                        return;
                     }
-                    follow_beam(mirror_map, light_map, x, y+1, Direction::Up);
+                    stacker::maybe_grow(0x8000, 0x100000, ||{
+                        follow_beam(mirror_map, light_map, x, y+1, Direction::Up, print);
+                    });
                 },
                 Direction::Left => {
                     if x == 0 {
-                        continue;
+                        return;
                     }
-                    follow_beam(mirror_map, light_map, x-1, y, Direction::Right);
+                    stacker::maybe_grow(0x8000, 0x100000, ||{
+                        follow_beam(mirror_map, light_map, x-1, y, Direction::Right, print);
+                    });
                 },
                 Direction::Right => {
                     if x == mirror_map[0].len() - 1 {
-                        continue;
+                        return;
                     }
-                    follow_beam(mirror_map, light_map, x+1, y, Direction::Left);
+                    stacker::maybe_grow(0x8000, 0x100000, ||{
+                        follow_beam(mirror_map, light_map, x+1, y, Direction::Left, print);
+                    });
                 }
             }
         }
-    }
+    });
 }
 
-fn print_light_map(light_map: &Vec<Vec<Vec<Direction>>>) {
+fn _print_light_map(light_map: &Vec<Vec<BTreeSet<Direction>>>) {
     for row in light_map {
         for col in row {
             if col.len() > 0 {
@@ -101,7 +135,27 @@ fn print_light_map(light_map: &Vec<Vec<Vec<Direction>>>) {
     }
 }
 
-fn count_light_map(light_map: &Vec<Vec<Vec<Direction>>>) -> usize {
+fn print_map(mirror_map: &Vec<Vec<char>>) -> Result<(),std::io::Error>{
+    let mut stdout = std::io::stdout();
+    stdout.execute(cursor::MoveTo(0,0))?;
+
+    for row in mirror_map {
+        for col in row {
+            match col {
+                '.' => {stdout.execute(style::PrintStyledContent(" ".white()))?;},
+                '\\' => {stdout.execute(style::PrintStyledContent("\\".dark_yellow()))?;},
+                '/' => {stdout.execute(style::PrintStyledContent("/".dark_yellow()))?;},
+                '|' => {stdout.execute(style::PrintStyledContent("|".dark_green()))?;},
+                '-' => {stdout.execute(style::PrintStyledContent("-".dark_green()))?;},
+                 _ => panic!("eh?")
+            }
+        }
+        stdout.execute(cursor::MoveToNextLine(1))?;
+    }
+    Ok(())
+}
+
+fn count_light_map(light_map: &Vec<Vec<BTreeSet<Direction>>>) -> usize {
     let mut sum = 0;
     for row in light_map {
         for col in row {
@@ -113,48 +167,45 @@ fn count_light_map(light_map: &Vec<Vec<Vec<Direction>>>) -> usize {
     sum
 }
 
-fn init_light_map(mirror_map: &Vec<Vec<char>>) -> Vec<Vec<Vec<Direction>>> {
-    let mut light_map: Vec<Vec<Vec<Direction>>> = Vec::new();
+fn init_light_map(mirror_map: &Vec<Vec<char>>) -> Mutex<Vec<Vec<BTreeSet<Direction>>>> {
+    let mut light_map: Vec<Vec<BTreeSet<Direction>>> = Vec::new();
     for row in 0..mirror_map.len() {
         light_map.push(Vec::new());
         for _col in 0..mirror_map[0].len() {
-            light_map[row].push(Vec::new());
+            light_map[row].push(BTreeSet::new());
         }
     }
-    light_map
+    Mutex::new(light_map)
 }
 
 fn main() {
     let input = read_to_string(".\\src\\test.txt").unwrap();
     let map: Vec<Vec<char>> = input.lines().map(|x|x.chars().collect()).collect();
 
-    let mut light_map = init_light_map(&map);
+    let _ = print_map(&map);
 
-    stacker::grow(0x10000000, ||{
-        follow_beam(&map, &mut light_map, 0, 0, Direction::Left);
-    });
+    let light_map = init_light_map(&map);
 
-    print_light_map(&light_map);
+    follow_beam(&map, &light_map, 0, 0, Direction::Left, true);
+     let mut stdout = std::io::stdout();
+     let _ = stdout.execute(cursor::MoveTo(0, map.len() as u16));
+    //_print_light_map(&light_map.lock().unwrap());
 
-    println!("q1: {:}", count_light_map(&light_map));
+    //println!("q1: {:}", count_light_map(&light_map.lock().unwrap()));
 
     let mut best_beam_coverage = 0;
     for x in 0..map[0].len() {
-        let mut light_map = init_light_map(&map);
-        stacker::grow(0x10000000, ||{
-            follow_beam(&map, &mut light_map, x, 0, Direction::Up);
-        });
-        let coverage = count_light_map(&light_map); {
+        let light_map = init_light_map(&map);
+        follow_beam(&map, &light_map, x, 0, Direction::Up, false);
+        let coverage = count_light_map(&light_map.lock().unwrap()); {
             if coverage > best_beam_coverage {
                 best_beam_coverage = coverage;
             }
         }
 
-        let mut light_map = init_light_map(&map);
-        stacker::grow(0x10000000, ||{
-            follow_beam(&map, &mut light_map, x, map.len()-1, Direction::Down);
-        });
-        let coverage = count_light_map(&light_map); {
+        let light_map = init_light_map(&map);
+        follow_beam(&map, &light_map, x, map.len()-1, Direction::Down, false);
+        let coverage = count_light_map(&light_map.lock().unwrap()); {
             if coverage > best_beam_coverage {
                 best_beam_coverage = coverage;
             }
@@ -162,27 +213,23 @@ fn main() {
     }
 
     for y in 0..map.len() {
-        let mut light_map = init_light_map(&map);
-        stacker::grow(0x10000000, ||{
-            follow_beam(&map, &mut light_map, 0, y, Direction::Left);
-        });
-        let coverage = count_light_map(&light_map); {
+        let light_map = init_light_map(&map);
+        follow_beam(&map, &light_map, 0, y, Direction::Left, false);
+        let coverage = count_light_map(&light_map.lock().unwrap()); {
             if coverage > best_beam_coverage {
                 best_beam_coverage = coverage;
             }
         }
 
-        let mut light_map = init_light_map(&map);
-        stacker::grow(0x10000000, ||{
-            follow_beam(&map, &mut light_map, map[0].len()-1, y, Direction::Right);
-        });
-        let coverage = count_light_map(&light_map); {
+        let light_map = init_light_map(&map);
+        follow_beam(&map, &light_map, map[0].len()-1, y, Direction::Right, false);
+        let coverage = count_light_map(&light_map.lock().unwrap()); {
             if coverage > best_beam_coverage {
                 best_beam_coverage = coverage;
             }
         }
     }
 
-    println!("q2: {:}", best_beam_coverage);
+    //println!("q2: {:}", best_beam_coverage);
 
 }
