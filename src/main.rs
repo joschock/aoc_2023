b5a465d369e9d2581c5b43cd88f535781327b6ce
John Schock <joschock@microsoft.com>
2023-12-27 12:33:04 -0800

    pt2 attempt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, collections::{BTreeMap, BTreeSet}};
 
 fn next_nodes(map: &Vec<Vec<char>>, x: usize, y: usize, slopes: bool) -> Vec<(usize, usize)> {
     if slopes {
@@ -63,32 +63,111 @@ fn next_nodes(map: &Vec<Vec<char>>, x: usize, y: usize, slopes: bool) -> Vec<(us
     .collect()
 }
 
-fn plot_route(map: &Vec<Vec<char>>, path: &Vec<(usize, usize)>, end: (usize, usize), slopes: bool) -> usize {
-    let (x, y) = *path.last().unwrap();
-    //println!("{:?}", (x, y));
-
-    if (x, y) == end {
-        //return vec![(x,y)];
-        return 1;
-    }
-
-    let mut routes = Vec::new();
-    for node in next_nodes(map, x, y, slopes) {
-        if !path.contains(&(node.0, node.1)) {
-            let mut next_path = path.clone();
-            next_path.push((node.0, node.1));
-            routes.push(plot_route(map, &next_path, end, slopes));
+fn find_next_branch(
+    map: &Vec<Vec<char>>,
+    visited: &mut BTreeSet<(usize, usize)>,
+    start: &(usize, usize),
+    end: (usize, usize),
+    slopes: bool
+) -> Option<((usize, usize), usize)>
+{
+    //println!("   find_branch: {:?}", start);
+    let mut current = *start;
+    let mut current_path = vec![current];
+    let mut candidates;
+    loop {
+        candidates = next_nodes(map, current.0, current.1, slopes);
+        //println!("       candidates: {:?}, current: {:?}", candidates, current);
+        candidates.retain(|x|!visited.contains(x) && !current_path.contains(x));
+        if candidates.len() != 1  {
+            break;
         }
+        current = candidates.pop().unwrap();
+        current_path.push(current);
+    }
+    //println!("   final candidates: {:?}, current: {:?}", candidates, current);
+
+    if candidates.len() != 0 || current == end{
+        Some(((current.0, current.1), current_path.len()))
+    } else {
+        None
+    }
+}
+
+
+fn condense_map(
+    map: &Vec<Vec<char>>,
+    points: &mut BTreeMap<(usize, usize),BTreeSet<((usize, usize), usize)>>,
+    visited: &mut BTreeSet<(usize, usize)>,
+    next: (usize, usize),
+    end: (usize, usize),
+    slopes: bool
+) {
+
+    //println!("next: {:?}", next);
+    let candidates = next_nodes(map, next.0, next.1, slopes);
+
+    let candidates:BTreeSet<((usize, usize), usize)> = candidates.iter()
+        .filter_map(
+            |x|{
+                find_next_branch(map, visited, x, end, slopes)
+            }
+        ).collect();
+
+    //println!("  candidates: {:?}", candidates);
+    if !points.contains_key(&next) {
+        points.insert(next, BTreeSet::new());
     }
 
-    let mut max_route = 0;
-    for route in routes {
-        if route > max_route {
-            max_route = route;
+    for candidate  in candidates {
+        visited.insert(candidate.0);
+        if !points.contains_key(&candidate.0) {
+            points.insert(candidate.0, BTreeSet::new());
         }
+
+        points.get_mut(&next).unwrap().insert(candidate);
+        points.get_mut(&candidate.0).unwrap().insert(((next.0, next.1),candidate.1));
+
+        condense_map(map, points, visited, candidate.0, end, slopes);
+        visited.remove(&candidate.0);
+    }
+}
+
+fn plot_longest_route(
+    points: &mut BTreeMap<(usize, usize),BTreeSet<((usize, usize), usize)>>,
+    visited: &mut BTreeSet<(usize, usize)>,
+    next: (usize, usize),
+    end: (usize, usize),
+) -> Option<(usize, Vec<(usize, usize)>)> {
+    if visited.contains(&next) {
+        return None;
     }
 
-    max_route + 1
+    if next == end {
+        //println!("found end");
+        return Some((next.1, vec![end]));
+    }
+
+    visited.insert(next);
+
+    let mut longest_route = 0;
+    let mut longest_path = Vec::new();
+    let nodes = points.get(&next).unwrap().clone();
+    //println!("next: {:?}\n\tnodes: {:?}", next, nodes);
+    for node in nodes {
+        //stacker::maybe_grow(0x8000, 0x100000, ||{
+            if let Some((route_len, path)) = plot_longest_route(points, visited, node.0, end) {
+                if route_len > longest_route  && path.contains(&end) {
+                    longest_route = route_len;
+                    longest_path = path;
+                }
+            }
+        //});
+    }
+
+    visited.remove(&next);
+    longest_path.push(next);
+    Some((longest_route + next.1, longest_path))
 }
 
 fn main() {
@@ -100,8 +179,34 @@ fn main() {
 
     println!("start: {:?}, end: {:?}", start, end);
     println!("map start: {:?}, map end: {:?}", map[start.1][start.0], map[end.1][end.0]);
-    stacker::grow(0x10000000, ||{
-        let longest_route = plot_route(&map, &vec![start], end, true);
-        println!("q1: {:}", longest_route -1);
-    });
+
+    let slopes = true;
+    let mut visited:BTreeSet<(usize, usize)>  = BTreeSet::new();
+    let first_branch = find_next_branch(&map, &mut visited, &start, end, slopes).unwrap();
+    let mut condensed:BTreeMap<(usize, usize),BTreeSet<((usize, usize), usize)>> = BTreeMap::new();
+    let mut first_branch_set = BTreeSet::new();
+    first_branch_set.insert(first_branch);
+    condensed.insert(start, first_branch_set);
+    visited.insert(first_branch.0);
+    condense_map(&map, &mut condensed, &mut visited, first_branch.0, end,slopes);
+
+    for (key, value) in &condensed {
+       println!("point: {:?}", key);
+       println!("  edges: {:?}", value);
+    }
+
+    let (longest_route, mut path) = plot_longest_route(&mut condensed, &mut BTreeSet::new(), start, end).unwrap();
+    path.reverse();
+    println!("longest_route: {:}, {:}, {:?}", longest_route, path.len(), path);
+
+    let mut sum = 0;
+    for idx in 0..path.len()-1 {
+        let node = path[idx];
+        let next_node = path[idx+1];
+        let edges = condensed.get(&node).unwrap();
+        let edge = edges.iter().find(|(x, _)| *x == next_node).unwrap();
+        sum += edge.1;
+        println!("node: {:?} next: {:?} length: {:?}, sum: {:?}", node, next_node, edge.1, sum);
+    }
+
 }
